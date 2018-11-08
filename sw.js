b1afccee5be260d3cfc2d0127d396dadbb6cdf9c
jialcaordev <44852960+jialcaordev@users.noreply.github.com>
2018-11-08 11:25:26 -0500

    Front

---
+++ sw.js
@@ -0,0 +1,91 @@
+// imports
+importScripts('https://cdn.jsdelivr.net/npm/pouchdb@7.0.0/dist/pouchdb.min.js')
+
+importScripts('js/sw-db.js');
+importScripts('js/sw-utils.js');
+
+// Escuchar PUSH
+self.addEventListener('push', e => {
+
+    // console.log(e);
+
+    const data = JSON.parse( e.data.text() );
+
+    // console.log(data);
+
+
+    const title = data.titulo;
+    const options = {
+        body: data.cuerpo,
+        // icon: 'img/icons/icon-72x72.png',
+        icon: `img/avatars/${ data.usuario }.jpg`,
+        badge: 'img/favicon.ico',
+        image: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/5b/Torre_de_los_Avengers.png/revision/latest?cb=20150626220613&path-prefix=es',
+        vibrate: [125,75,125,275,200,275,125,75,125,275,200,600,200,600],
+        openUrl: '/',
+        data: {
+            // url: 'https://google.com',
+            url: '/',
+            id: data.usuario
+        },
+        actions: [
+            {
+                action: 'thor-action',
+                title: 'Thor',
+                icon: 'img/avatar/thor.jpg'
+            },
+            {
+                action: 'ironman-action',
+                title: 'Ironman',
+                icon: 'img/avatar/ironman.jpg'
+            }
+        ]
+    };
+
+
+    e.waitUntil( self.registration.showNotification( title, options) );
+
+
+});
+
+
+// Cierra la notificacion
+self.addEventListener('notificationclose', e => {
+    console.log('Notificación cerrada', e);
+});
+
+
+self.addEventListener('notificationclick', e => {
+
+
+    const notificacion = e.notification;
+    const accion = e.action;
+
+
+    console.log({ notificacion, accion });
+    // console.log(notificacion);
+    // console.log(accion);
+    
+
+    const respuesta = clients.matchAll()
+    .then( clientes => {
+
+        let cliente = clientes.find( c => {
+            return c.visibilityState === 'visible';
+        });
+
+        if ( cliente !== undefined ) {
+            cliente.navigate( notificacion.data.url );
+            cliente.focus();
+        } else {
+            clients.openWindow( notificacion.data.url );
+        }
+
+        return notificacion.close();
+
+    });
+
+    e.waitUntil( respuesta );
+
+
+});
